cae1f26ebe7642a3d84d924d7e3d1b9be88432d7
CemileIsmayilZada <5cctf7f@code.edu.az>
2022-10-31 21:35:19 +0400

    my codes

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+int givenNumber = 123;
+int myNumber=givenNumber
+int lastNumber = 0;
+int total = 0;
+
+if (givenNumber < 0)
+{
+    Console.WriteLine("musbet eded daxil edin!");
+}
+else if (givenNumber == 0)
+{
+    Console.WriteLine("0");
+}
+else
+{
+    while (givenNumber > 0)
+    {
+        lastNumber = givenNumber % 10; // 3 2
+        givenNumber = givenNumber / 10; // 123  12  1
+        total = total + lastNumber;  // 3  5  6
+
+    }
+    Console.WriteLine("Sum of numbers for " + myNumber + " : " + total); // 6
+}
---
+++ Task2Codes/Task2Codes/Program.cs
@@ -0,0 +1,30 @@
+﻿class Task2Codes
+{
+
+    
+    static void Main(String[] args)
+    {
+        int number=1;
+        int sum=0;
+        int mean = 0;
+        int i = 0;
+    
+        while (number < 10)
+        {
+            
+            if (number / 2 == 0) 
+
+            {
+                i++; // 1 2 3 4
+                sum = sum + number; //  2  6 12 20
+
+                mean = sum / i;  // 2  3  4  5
+            }
+            number++; // 2 4 6 8 -
+        }
+
+        Console.WriteLine("mean " +  mean); // 5
+
+    }
+
+}
